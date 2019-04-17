37125918633610be8dd2f58094a60aa49907cdf2
Jingwei Sun <sunjingw@sheridancollege.ca>
2019-04-17 18:29:09 -0400

    small change to the enum

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -25,9 +25,16 @@ public class Card
         SPADES, HEART, CLUBS, DIAMOND
     }
         
+    
+    //trying to add integer values to the enum so they can be compared
     public enum Ranks {
-        ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
-        TEN, JACK, QUEEN, KING
+        ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
+        TEN(10), JACK(11), QUEEN(12), KING(13);
+        
+        private int value;
+        Ranks(int value) {
+            this.value = value;
+        }
     }
     
     private Suits s;
--- src/ca/sheridancollege/project/War.java
+++ src/ca/sheridancollege/project/War.java
@@ -12,13 +12,8 @@ import java.util.Scanner;
  *
  * @author CS
  */
-public class War extends Game {
-    public War() {
-        super("War");
-        
-    }
-
-
+public class War {
+    
 public static void main(String[] args) {
 
     Game game = new Game("War");
@@ -58,34 +53,6 @@ public static void main(String[] args) {
     
     
     
-    
-    /*
-    
-    
-    // initialize deck
-        String[] deck = new String[n];
-        for (int i = 0; i < RANKS.length; i++) {
-            for (int j = 0; j < SUITS.length; j++) {
-                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
-            }
-        }
-
-        // shuffle
-        for (int i = 0; i < n; i++) {
-            int r = i + (int) (Math.random() * (n-i));
-            String temp = deck[r];
-            deck[r] = deck[i];
-            deck[i] = temp;
-        }
-
-        // print shuffled deck
-        for (int i = 0; i < PLAYERS * CARDS_PER_PLAYER; i++) {
-            System.out.println(deck[i]);
-            if (i % CARDS_PER_PLAYER == CARDS_PER_PLAYER - 1)
-                System.out.println();
-        }
-        
-        */
     
 }
 }
